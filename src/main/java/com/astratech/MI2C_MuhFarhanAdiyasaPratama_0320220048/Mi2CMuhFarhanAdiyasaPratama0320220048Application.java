119470accea8c2efc82235f0727b124ca322f5ec
astratechlearn <adiyasaa56@gmail.com>
2024-06-21 07:43:11 +0700

    Initial commit

---
+++ src/main/java/com/astratech/MI2C_MuhFarhanAdiyasaPratama_0320220048/Mi2CMuhFarhanAdiyasaPratama0320220048Application.java
@@ -0,0 +1,13 @@
+package com.astratech.MI2C_MuhFarhanAdiyasaPratama_0320220048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mi2CMuhFarhanAdiyasaPratama0320220048Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mi2CMuhFarhanAdiyasaPratama0320220048Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/astratech/MI2C_RizkyMuliaPerdana_0320220023/Mi2CRizkyMuliaPerdana0320220023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astratech.MI2C_MuhFarhanAdiyasaPratama_0320220048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mi2CMuhFarhanAdiyasaPratama0320220048ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
